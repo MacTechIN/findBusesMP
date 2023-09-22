7656801e5eaccbae0a53d73f3f85c3fa7fda5c4f
ungna <jun910@naver.com>
2023-09-22 09:45:20 +0900

    update: 깔끔하게

--- bus.py
+++ bus.py
@@ -7,13 +7,12 @@ Created on Thu Sep 21 17:11:43 2023
 
 # 모듈 import
 import requests
-import pprint
 
 #인증키 입력
-encoding = "%2BltohkyQC0eQUMVVaH5qwUi4FxaROssy0kpwzEdkqsFqedo%2FKlvT05Ap0svSUr2xQsOHd9%2FK2pXWpnH5N%2BmTcg%3D%3D"
-decoding = '+ltohkyQC0eQUMVVaH5qwUi4FxaROssy0kpwzEdkqsFqedo/KlvT05Ap0svSUr2xQsOHd9/K2pXWpnH5N+mTcg=='
+encoding = "MsMlVXwTa6iJaepslzIENgYMrdmGndKRzvqoMgWnBH2K2kUV0xJB%2FM%2BdHc1zFvKBSXkP2RoS9DQQqYUNrbjAQg%3D%3D"
+decoding = "MsMlVXwTa6iJaepslzIENgYMrdmGndKRzvqoMgWnBH2K2kUV0xJB/M+dHc1zFvKBSXkP2RoS9DQQqYUNrbjAQg=="
 
-# 학원 = "200000093"
+## 학원 = "200000093"
 ## 집앞 =  "233000074"
 station_id = "200000093"
 
@@ -24,46 +23,51 @@ params ={'serviceKey' : decoding, 'stationId' : station_id }
 response = requests.get(url, params=params)
 
 
+#%%
 # xml 내용넣기
 content = response.text
 
-
-### xml을 DataFrame으로 변환하기 ###
-from os import name
-import xml.etree.ElementTree as et
+# xml을 DataFrame으로 변환하기
 import pandas as pd
-import bs4
-from lxml import html
-from urllib.parse import urlencode, quote_plus, unquote
-
+from bs4 import BeautifulSoup
 
 #bs4 사용하여 busArrivalList 태그 분리
 
-xml_obj = bs4.BeautifulSoup(content,'lxml-xml')
+xml_obj = BeautifulSoup(content,'lxml-xml')
 rows = xml_obj.findAll('busArrivalList')
 
-
+#%%
 # 모든 행과 열의 값을 모아 dataframe으로 만들기
 
 rowList = []
+textList = []
 nameList = []
-columnList = []
 
+# nameList 만들기
+# 어차피 동일한 columns을 가지기떄문에 nameList는  한번만 해도됨
+column = rows[0].find_all()
+for i in range(0, len(column)):
+        nameList.append(column[i].name)
+        # 컬럼값은 모든 행의 값을 저장해야한다.    
+
+
+# rowList에 각 row별 내용넣기
 for i in range(0, len(rows)):
     columns = rows[i].find_all()
     
     for j in range(0, len(columns)):
-        # 어차피 동일한 columns을 가지기떄문에 nameList는  한번만 해도됨
-        if i == 0:
-            nameList.append(columns[j].name)
-        # 컬럼값은 모든 행의 값을 저장  
+        columnList = []
+        # 컬럼값은 모든 행의 값을 저장해야한다.    
         eachColumn = columns[j].text
-        columnList.append(eachColumn)
-    rowList.append(columnList)
-    columnList = []    # 다음 row의 값을 넣기 위해 비워야됨 
+        textList.append(eachColumn)
+    rowList.append(textList)
+    textList = []  # 다음 row의 값을 넣기 위해 비워준다. 
+       
     
 result = pd.DataFrame(rowList, columns=nameList)
 result.head()
 
+
+#%%
 # 원하는 부분만 뺴내기
-final_result = result[['plateNo1','plateNo2','predictTime1','predictTime2']]
+final_result = result[['plateNo1','plateNo2','predictTime1','predictTime2', 'routeId']]
