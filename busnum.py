e3744d4a7df0f7d2a434906dc9fb8e1d6943c595
ungna <jun910@naver.com>
2023-09-22 09:47:30 +0900

    routeId를 이용해 버스번호 추출

--- busnum.py
+++ busnum.py
@@ -11,7 +11,7 @@ url = "http://apis.data.go.kr/6410000/busrouteservice/getBusRouteInfoItem"
 encoding = "MsMlVXwTa6iJaepslzIENgYMrdmGndKRzvqoMgWnBH2K2kUV0xJB%2FM%2BdHc1zFvKBSXkP2RoS9DQQqYUNrbjAQg%3D%3D"
 decoding = "MsMlVXwTa6iJaepslzIENgYMrdmGndKRzvqoMgWnBH2K2kUV0xJB/M+dHc1zFvKBSXkP2RoS9DQQqYUNrbjAQg=="
  # 암거나 한 넣음
-routeid = "200000049"  
+routeid = "200000315"  
 
 params = {"serviceKey" : decoding, "routeId" : routeid}
 
@@ -19,6 +19,25 @@ params = {"serviceKey" : decoding, "routeId" : routeid}
 
 response = requests.get(url, params = params)
 
+
+#%%
+# xml 내용넣기
 content = response.text
 
+# xml을 DataFrame으로 변환하기
+from bs4 import BeautifulSoup
+
+#bs4 사용하여 busArrivalList 태그 분리
+
+xml_obj = BeautifulSoup(content,'lxml-xml')
 # routeName 우리가 아는 버스번호
+rows = xml_obj.find('routeName')
+
+# 버스번호 추출
+bus_name = rows.text
+
+
+
+
+
+
