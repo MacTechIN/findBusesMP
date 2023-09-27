0b40131bf0fce1f7cdb7ddb4fe448f0e825f093a
MacTechIN <wooriszhome@gmail.com>
2023-09-27 09:03:06 +0900

    add alpha folder for new updated findmybusinfo

--- WebService/buttontest.py
+++ WebService/buttontest.py
@@ -4,3 +4,17 @@ i1 = st.button("button 1")
 st.write("value:", i1)
 
 i2 = st.checkbox("reset button")
+
+
+import streamlit as st
+
+st.title("Streamlit Test")
+input_user_name = st.text_input(label="User Name", value="")
+
+checkbox = st.checkbox('agree')
+btn_clicked = st.button("Confirm", key='confirm_btn', disabled=(checkbox is False))
+
+if btn_clicked:
+    con = st.container()
+    con.caption("Result")
+    con.write(f"Hello~ {str(input_user_name)}")
